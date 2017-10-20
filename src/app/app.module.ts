bfea8386a034a39d7b18d8ce1090ff74494897dc
christianarredondo <christianarredondo@utexas.edu>
2017-10-19 21:48:02 -0500

    install and implement Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {
+  MatToolbarModule
+} from '@angular/material';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
