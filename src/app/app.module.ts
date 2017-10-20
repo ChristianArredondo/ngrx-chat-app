bd2aedd6a0f76824fb6129541ec4d076806549e5
christianarredondo <christianarredondo@utexas.edu>
2017-10-19 22:00:55 -0500

    generate main components and install flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -7,10 +8,16 @@ import { AppComponent } from './app.component';
 import {
   MatToolbarModule
 } from '@angular/material';
+import { UserSelectionComponent } from './user-selection/user-selection.component';
+import { ThreadSectionComponent } from './thread-section/thread-section.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserSelectionComponent,
+    ThreadSectionComponent,
+    MessageSectionComponent,
   ],
   imports: [
     BrowserAnimationsModule,
